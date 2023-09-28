337f4ed01297d030015e55d08ff2179ced1990b1
BaconScripter <125394392+thanhdat4461@users.noreply.github.com>
2023-09-28 15:27:59 +0700

    Update and rename LOADSTRINGS.lua to LOADSTRING.lua

---
+++ LOADSTRING.lua
@@ -0,0 +1,9 @@
+-- Speed Run 4
+if game.PlaceId == 183364845 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thanhdat4461/BaconScripterHub/main/Games/Speed%20Run%204.lua"))()
+elseif game.PlaceId == 13827198708 then
+         
+else
+game:GetService("StarterGui"):SetCore("SendNotification", { Title = "The Game Is Not Supported!"; Text = "Please Play Game That Support Hub"; Button1 = "Okay"; Duration = 60 })     
+})
+end
